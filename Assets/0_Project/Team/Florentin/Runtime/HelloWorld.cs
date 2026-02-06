91d3926e7f7bee6a32aa3a2b4123719b21ec6348
Florentin Dhenau <florentin.dhenau@gmail.com>
2026-02-06 11:46:11 +0100

    Fixed assembly name and path xbox mono

--- Assets/0_Project/Team/Florentin/Runtime/HelloWorld.cs
+++ Assets/0_Project/Team/Florentin/Runtime/HelloWorld.cs
@@ -3,19 +3,7 @@ using UnityEngine;
 namespace Florentin.ArcNES
 {
     public class HelloWorld : MonoBehaviour
-    
-   // [SerializeField] private XboxMono_XboxWithCode m_xbox;
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+   private XboxMono_XboxWithCode m_xbox;
     }
 }
